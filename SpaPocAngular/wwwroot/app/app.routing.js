e76766b406ff5f96e6a3e3d971387b44a8c4c6c1
mskibaSkiba <mskiba@3ecompany.com>
2018-02-25 12:59:52 -0800

    Cleanded out old test projects
    Signed-off-by: mskibaSkiba <mskiba@3ecompany.com>

--- SpaPocAngular/wwwroot/app/app.routing.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var router_1 = require("@angular/router");
-var routes = [
-    //{ path: '', component: SplashComponent },
-    { path: 'app', loadChildren: 'app/home/home.module#HomeModule' }
-];
-exports.routing = router_1.RouterModule.forRoot(routes);
-//# sourceMappingURL=app.routing.js.map
